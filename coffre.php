764118bb1af009d707ab9e0b0c858a82ad2d0c2c
DuperierLucas <mr.duperier73@gmail.com>
2022-04-29 11:06:25 +0200

    coffre

---
+++ coffre.php
@@ -0,0 +1,8 @@
+<?php include "functions.php";
+?>
+<link rel="stylesheet" href="coffre.css">
+
+<div class="coffre">
+    <div class="coffre-header">Coffres :</div>
+    <div class="coffre-content" id="bas-droite-contenu"><?php echo getScore(); ?></div>
+</div>
