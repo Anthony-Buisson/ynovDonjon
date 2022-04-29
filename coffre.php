3b6b0a8279523ea1b9aadff7093900f5ddc7896a
DuperierLucas <mr.duperier73@gmail.com>
2022-04-29 13:49:42 +0200

    Style

--- coffre.php
+++ coffre.php
@@ -1,6 +1,6 @@
 <div class="coffre">
     <div id="coffre-div">
         <div class="coffre-header">Coffres :</div>
-        <div class="coffre-content" id="bas-droite-contenu"><?php echo getScore(); ?></div>
+        <div class="coffre-content"><?php echo getScore(); ?></div>
     </div>
 </div>
